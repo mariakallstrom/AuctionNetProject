94aca4bffd379760be3b61fc6dcd3a88c5b4a1d2
annasandberg <Anna.Kallman@yh.nackademin.se>
2016-11-15 20:19:50 +0100

    Skapa formulär och interface

---
+++ AuctionNet/Forms/FrmAddCustomer.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace AuctionNet.Forms
+{
+    partial class FrmAddCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtCustomerTelephone = new System.Windows.Forms.TextBox();
+            this.lblTelephone = new System.Windows.Forms.Label();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(23, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Namn";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(26, 55);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(158, 22);
+            this.txtCustomerName.TabIndex = 1;
+            // 
+            // txtCustomerEmail
+            // 
+            this.txtCustomerEmail.Location = new System.Drawing.Point(26, 136);
+            this.txtCustomerEmail.Name = "txtCustomerEmail";
+            this.txtCustomerEmail.Size = new System.Drawing.Size(158, 22);
+            this.txtCustomerEmail.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(23, 104);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtCustomerTelephone
+            // 
+            this.txtCustomerTelephone.Location = new System.Drawing.Point(26, 220);
+            this.txtCustomerTelephone.Name = "txtCustomerTelephone";
+            this.txtCustomerTelephone.Size = new System.Drawing.Size(158, 22);
+            this.txtCustomerTelephone.TabIndex = 5;
+            // 
+            // lblTelephone
+            // 
+            this.lblTelephone.AutoSize = true;
+            this.lblTelephone.Location = new System.Drawing.Point(23, 188);
+            this.lblTelephone.Name = "lblTelephone";
+            this.lblTelephone.Size = new System.Drawing.Size(56, 17);
+            this.lblTelephone.TabIndex = 4;
+            this.lblTelephone.Text = "Telefon";
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(246, 120);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(135, 55);
+            this.btnAddCustomer.TabIndex = 6;
+            this.btnAddCustomer.Text = "Spara Kund";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            // 
+            // FrmAddCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 274);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Controls.Add(this.txtCustomerTelephone);
+            this.Controls.Add(this.lblTelephone);
+            this.Controls.Add(this.txtCustomerEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmAddCustomer";
+            this.Text = "Lägg till Kund";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtCustomerTelephone;
+        private System.Windows.Forms.Label lblTelephone;
+        private System.Windows.Forms.Button btnAddCustomer;
+    }
+}
