8ffff71dd3266a1853e7b9e8db69a732d82ace55
Maria <maria.kallstrom@nackademin.se>
2016-11-16 11:20:36 +0100

    Add files via upload

--- History-procedure.sql
+++ History-procedure.sql
@@ -7,6 +7,7 @@ BEGIN
 	declare @SupplierName NVARCHAR(50);
 	declare @SupplierId INT;
 	declare @CustomerName varchar(50);
+	declare @CustomerId INT;
 	declare @StartPrice DECIMAL;
 	declare @EndPrice DECIMAL
 
@@ -22,12 +23,19 @@ BEGIN
 	SET @SupplierName = (Select Suppliers.Name FROM Suppliers WHERE Suppliers.Id = @SupplierId)
 	SET @EndPrice = (Select MAX(bid) FROM Bids WHERE Bids.AuctionId = @AuctionId)
 	SET @StartPrice = (Select Products.StartPrice FROM Products WHERE Products.Id = @ProductId)
+	SET @CustomerId = (SELECT CustomerId FROM
+						(
+							SELECT TOP 1 * FROM(
+							SELECT MAX(Bid) as [Max bid], CustomerId FROM Bids
+							WHERE bids.AuctionId = 1
+							GROUP BY CustomerId) a) a)
+	SET @CustomerName = (SELECT Customers.Name FROM Customers WHERE Customers.Id = @CustomerId)
 
 
 
 		insert into AuctionHistory
 (AuctionNumber,ProductName,SuplierName,StartPrice,EndPrice,CustomerName) 
-	values(@AuctionId,@ProductName,@SupplierName,@StartPrice,@EndPrice,'TOMT');
+	values(@AuctionId,@ProductName,@SupplierName,@StartPrice,@EndPrice,@CustomerName);
 
 	PRINT 'Fanns en köpare, lägger till i Historik'
 END
